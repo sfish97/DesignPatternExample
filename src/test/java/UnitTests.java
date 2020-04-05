a7cd522cd7c30d988a33aafc67a0bb8af1614d1b
SFish97 <sfishbough@gmail.com>
2020-04-05 15:53:47 -0700

    Setup: Gradle, Spotbugs, Checkstyle, Unit Tests

---
+++ src/test/java/UnitTests.java
@@ -0,0 +1,28 @@
+package test.java;
+
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UnitTests {
+    
+    @Before
+    public void setUp() throws Exception {
+
+    }
+    
+    @After
+    public void tearDown() throws Exception {
+
+    }
+    
+    @Test
+    public void doFail() {
+        fail();
+    }
+     
+}
